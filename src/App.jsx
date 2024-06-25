37a40bedd21eee0296e74776fd9b0ef6ff578c91
xkl <joselucas20048jl@gmail.com>
2024-06-25 12:56:04 -0300

    update -> mobile galery done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import { Contents } from "./Components/Content/Contents"
+import { Photos } from "./Components/Photos/Photos"
 import { FHeader } from "./Components/Features/FHeader"
 import { Features } from "./Components/Features/Features"
 import { Header } from "./Components/Header/Header"
 import { Hero } from './Components/Hero/Hero'
 
-import macBook from './assets/MacbookPro.png'
 
 export const App = () => {
   return (
-    <div className="">
+    <div>
       <Header />
       <Hero mainText="Work at the speed of the thought"
         subHeading="Most calendars are designed for teams. Slate is designed for 
 freelancers who want a simple way to plan their schedule." />
       <Features>
         <FHeader />
-        <div className="mt-52 md:flex-row justify-center flex-col flex">
-          <Contents />
-        </div>
       </Features >
+      <div className="mt-52 md:flex-row justify-center flex-col flex">
+        <Contents />
+      </div>
+      <Photos text="Gallery" content="We focus on ergonomics and meeting
+ you where you work. 
+It's only a keystroke away." />
+
     </div >
   )
 }
